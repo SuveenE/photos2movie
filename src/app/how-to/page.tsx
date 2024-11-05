d25cb6befd5e2ed087bb9db8760c517f1b71cef2
SuveenE <suveen.te1@gmail.com>
2024-11-04 22:35:13 -0800

    Add how-to page

---
+++ src/app/how-to/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function HowTo() {
+  return (
+    <div className="flex bg-white text-black flex-col items-center justify-items-center min-h-screen p-12  pb-20 mx-auto">
+      <p className="text-xl font-bold">Create Your Photo Montage</p>
+      <p className="text-gray-600 mt-2">
+        Upload up to 50 images to create a video montage!
+      </p>
+    </div>
+  );
+}
