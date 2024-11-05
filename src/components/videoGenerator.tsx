07c5958d4e7004c4a0a615962eb6cf91b8d6681a
SuveenE <suveen.te1@gmail.com>
2024-11-04 22:05:27 -0800

    Update

--- src/components/videoGenerator.tsx
+++ src/components/videoGenerator.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import Image from "next/image";
